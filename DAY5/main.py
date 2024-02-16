aa33a60096a87cdb33a1498709690662061a7290
Daksh7112003 <13anil67@gmail.com>
2024-02-16 16:55:00 +0530

    Day5

---
+++ DAY5/main.py
@@ -0,0 +1,24 @@
+#  \n  (escape sequence character , basically this will the print value in 2 lines)
+
+#example
+print("hello world \n dhsjhdj")
+
+#output will be .....
+# hello world 
+#  dhsjhdj
+
+
+# thats how the content divides into 2 lines
+
+
+print("dakjhdhahd\nhdjhsdhfhhfh")
+
+
+
+'''      another way of commenting something in python         '''
+
+"""     one more           """
+
+
+
+print("Hey I m a good boy and \n rohan is stupid")
