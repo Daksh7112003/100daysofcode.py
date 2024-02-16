1961263551ee8cb8f3b9f1896e748119ce984d78
Daksh7112003 <13anil67@gmail.com>
2024-02-16 15:32:56 +0530

    Day1

---
+++ DAY1/main.py
@@ -0,0 +1,11 @@
+#programming is a way to tell computer what to do
+
+
+
+#installation of python is done on day 1 ...
+
+
+
+#Basic summary of day 1 (What is programming and python and how to install python on pc.... only theory  )
+
+
