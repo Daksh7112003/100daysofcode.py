23969f679ab94a739d0dc06d5476cbd96c8123aa
Daksh7112003 <13anil67@gmail.com>
2024-02-16 17:42:10 +0530

    Day6

---
+++ DAY6/main.py
@@ -0,0 +1,10 @@
+a = 1+2309
+b =True
+c = "string"
+print("The type of a is" , type(a))
+print("The type of b is" , type(b)) 
+print("The type of c is" , type(c))
+
+
+
+#everything is a object in python ..
