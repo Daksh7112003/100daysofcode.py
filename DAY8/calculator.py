3a207e20a0a1303cab219a25fa432603c6b12cb3
Daksh7112003 <13anil67@gmail.com>
2024-02-16 18:31:16 +0530

    Day8

---
+++ DAY8/calculator.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the input a : "))
+b = int(input("Enter the input b : "))
+
+print("The value of" , a , "+" , b, "is" , a+b)
+print("The value of " , a , "-" , b ,"is " , a-b)
+print("The value of " , a , "/", b , "is ", a/b)
+print("The value of " , a , "*", b , "is ", a*b)
+
+
+#Excercise 1 
