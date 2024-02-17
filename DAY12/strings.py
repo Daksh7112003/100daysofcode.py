eb38a7b533165812c88058cbaeb8241d8ee913cf
Daksh7112003 <13anil67@gmail.com>
2024-02-17 17:07:55 +0530

    Day12

---
+++ DAY12/strings.py
@@ -0,0 +1,29 @@
+name ="harry"
+friend =" Rohan"
+anotherFriend='lovish'
+
+apple=''' he said 
+bla bla and 
+blah
+'''
+print(name +friend  + apple)
+
+
+print(name[0])
+
+#this will going to print h
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+# print(name[5])# index out of bound.......
+
+
+
+# loops in strings ...
+
+
+for character in apple:
+    print(character)
+
+    #this will print all the character of the apple object in a prefect way ..... here we go again ........
