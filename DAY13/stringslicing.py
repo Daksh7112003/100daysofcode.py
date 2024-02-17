692d154b0a18c821f526d7d5932f0b5979918e56
Daksh7112003 <13anil67@gmail.com>
2024-02-17 17:50:11 +0530

    Day13

---
+++ DAY13/stringslicing.py
@@ -0,0 +1,22 @@
+a = "daksh,chhabra"
+print(len(a))
+
+
+print(a[0:4])  # wont include 4 char and its afterward
+
+
+#negative slicing......
+
+b="japan"
+print(b[0:-3])  #basically its length - value (in the case of negative slicing )
+
+print(b[0:len(b)-3]) # can also written in this format .....
+
+
+
+
+
+
+
+nm = "harry"
+print(nm[-4:-2])
