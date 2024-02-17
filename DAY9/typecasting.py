a326c028622d8354eba3cd96bcb82f41996ba0ae
Daksh7112003 <13anil67@gmail.com>
2024-02-17 16:34:24 +0530

    Day9

---
+++ DAY9/typecasting.py
@@ -0,0 +1,15 @@
+# Explicit typecasting ( i want to do the conversion)
+#implicit typecasting ( automatically conversion happens )
+ 
+
+
+
+
+#implicit typecasting
+
+a =0.9
+b =10
+print(a+b)
+
+
+# automatically convert the lower order variable into higher order
