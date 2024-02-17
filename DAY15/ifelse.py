4620733601800f8311978475abf537e41673411a
Daksh7112003 <13anil67@gmail.com>
2024-02-18 00:45:06 +0530

    Day15

---
+++ DAY15/ifelse.py
@@ -0,0 +1,41 @@
+apple_price=int(input("type the price "))
+budget =200
+
+if(apple_price<=budget):
+    print("lele bhai")
+else:
+    print("pagal h kya bhai")
+
+#.... the extra space is the indentification space ..........
+
+    #     basically in java ,c language we enter the object with the {
+
+    #     }
+    # but in the pyhton we just give some space .........
+
+
+
+
+    #elif
+
+
+apple_pricee=int(input("type the price "))
+budget =200
+
+
+
+if(budget-apple_pricee>90):
+        print("daba ke lele")
+elif(budget-apple_pricee>50):
+        print("thoda sa lele")
+else:
+        print("pagal h kya")
+
+
+
+
+#nested if ... same bla bla ...
+        
+
+
+        
