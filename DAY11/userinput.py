26d1561e310768c9856d087318acb6efb7d771c8
Daksh7112003 <13anil67@gmail.com>
2024-02-17 16:55:05 +0530

    Day11

---
+++ DAY11/userinput.py
@@ -0,0 +1,23 @@
+a = input("Enter your name ")
+print(a)
+
+#pyhton input is very poor in detecting the values , so thats everything as a string , so thats a problem.......
+
+
+x = input("Enter the number ")
+y = input("Enter the number ")
+
+print(x+y) # concatination....
+print(int(x)+int(y)) #arthemetic additon....
+
+
+
+#exercise
+print(x*y)  #cant multiply string 
+print(int(x)*int(y))
+print(x-y)  #not supported in the string .........
+print(int(x)-int(y))
+
+
+
+
