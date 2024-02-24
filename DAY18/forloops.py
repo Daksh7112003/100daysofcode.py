07d400021744e96a581aefd6c5f255c5212f55c5
Daksh7112003 <13anil67@gmail.com>
2024-02-24 17:06:25 +0530

    Day18

---
+++ DAY18/forloops.py
@@ -0,0 +1,27 @@
+for x in range(1,2):
+    print(x)
+
+
+name='Abhishek'
+for i in name:
+    print(i)
+
+
+    colors =["Red" , "Green" , "Blue" , "yellow"]
+    for color in colors:
+        print(color)
+        for i in color:
+            print(i)
+
+
+    for k in range(1,4):
+        print(k)     
+
+
+
+
+
+for k in range(1,12,3):
+    print(k)
+
+    
