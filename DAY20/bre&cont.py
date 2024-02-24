dd3362a6e99a3a0d6f5f4fd70115bb0e696b30d1
Daksh7112003 <13anil67@gmail.com>
2024-02-24 19:27:25 +0530

    Day19

---
+++ DAY20/bre&cont.py
@@ -0,0 +1,19 @@
+# break the matlab loop ko chrr ke nikal jayo
+
+
+#continue ka matlab iteration ko chrr ke nikal jayo...
+
+
+
+
+for i in range(12):
+    if(i==10):
+        print("loop ko chrke nikal gya")
+        break
+
+    if(i==5):
+        print("iteration ko chrke bhar ho gya")
+        continue
+    print("5 X" , i+1 ,"=" , 5*(i+1))
+
+
