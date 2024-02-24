37441ee1e72f1f7b759dda3197f5034a7cc017d0
Daksh7112003 <13anil67@gmail.com>
2024-02-24 18:11:42 +0530

    Day19

---
+++ DAY19/while.py
@@ -0,0 +1,22 @@
+#while loops.....
+
+i = int(input("Enter the number:"))
+while(i<=38):
+ 
+ i = int(input("Enter the number:"))
+ print(i)
+
+
+print("Done with the loop")
+
+
+count =5
+while(count>0):
+ print(count)
+ count =count -1 
+
+
+
+
+
+
