c82ed54817ac3e7d487239ea3ec604e54ccb7ec7
Daksh7112003 <13anil67@gmail.com>
2024-02-29 19:11:25 +0530

    Day21

---
+++ DAY21/fxn.py
@@ -0,0 +1,30 @@
+ 
+def calculateGmean(a,b):
+   mean=(a*b)/(a+b)
+   print(mean)
+
+
+
+a = 9
+b = 8
+calculateGmean(a,b)
+
+
+
+c=8
+d=7
+calculateGmean(c,d)
+
+
+
+def isGreater(a,b):
+   if(a>b):
+      print("First number is greater")
+   else:
+      print("Second number is greater or equal")
+
+
+
+c=8
+d=6
+isGreater(c,d)
