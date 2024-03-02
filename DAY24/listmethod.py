d431f38bc04eef38b20ff6a993e03daa620e02b1
Daksh7112003 <13anil67@gmail.com>
2024-03-02 17:16:26 +0530

    list-methods

---
+++ DAY24/listmethod.py
@@ -0,0 +1,46 @@
+l=[1,23,4,5,5]
+
+l.append(4)  # for adding something in the list 
+
+
+
+print(l)
+
+
+
+
+
+l.sort() # for sorting .......
+
+l.sort(reverse=True) # for sorting in a reverse manner .........
+
+
+l=[1,23,4,5,5]
+
+
+
+
+
+print(l)
+l.reverse()  # for reverse=ing the list .......
+
+
+print(l)
+
+
+
+
+l.insert(1,8999)
+
+#1 is index no and 899 is the inserting value ..
+
+
+
+l =[199, 334,553]
+k=[32,45,6,64]
+i=k+l # jo phale lege voh pahle ayega .....
+
+
+
+print(i)
+
