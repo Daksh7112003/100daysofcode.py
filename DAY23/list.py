44ee505e672b68b3bdfa8c6ba065c5fb378fc596
Daksh7112003 <13anil67@gmail.com>
2024-03-02 16:58:56 +0530

    list

---
+++ DAY23/list.py
@@ -0,0 +1,33 @@
+marks =[2,4,5]
+print(marks)
+print(type(marks))
+print(marks[2])
+
+
+
+marks =[3, 5,6,"Harry" , True]
+
+print(marks[2])
+print(marks[1])
+
+
+
+
+
+
+  
+
+if "6" in marks:
+    print("Yes")
+else:
+    print("No")
+
+
+
+
+list =[2,3,4,5]
+print(list)
+
+list=[i for i in range(10)]
+ 
+print(list)
