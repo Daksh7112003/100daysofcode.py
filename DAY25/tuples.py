f0ab7e80d5721b49ec3c335e85e88d115576e43f
Daksh7112003 <13anil67@gmail.com>
2024-03-02 17:26:34 +0530

    DAY25

---
+++ DAY25/tuples.py
@@ -0,0 +1,21 @@
+# list can be changed but tupple cant 
+
+tup=(1,3,4,76 ,'green', True)
+
+print(type(tup), tup)
+
+print(len(tup))
+print(tup[-1])
+
+print(tup[0])
+
+print(tup[2])
+
+
+
+
+# as the tuple cant be edited so , so we slice in this way ...
+
+
+
+tup2=tup[1:3] 
