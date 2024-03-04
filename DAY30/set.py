bd24be71984e58a0b79c16e69c957b4e88c94238
Daksh7112003 <13anil67@gmail.com>
2024-03-04 13:06:22 +0530

    Day30

---
+++ DAY30/set.py
@@ -0,0 +1,14 @@
+s = {2, 4, 2, 6}
+print(s)
+
+info = {"Carla", 19, False, 5.9, 19}
+print(info)
+
+harry = set()
+print(type(harry))
+
+for value in info:
+  print(value)
+
+
+  #Sets are unordered collection of data items. They store multiple items in a single variable. Set items are separated by commas and enclosed within curly brackets {}. Sets are unchangeable, meaning you cannot change items of the set once created. Sets do not contain duplicate items.
