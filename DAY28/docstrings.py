9fea26e7205c9e959564a6079076e2d501f2bf89
Daksh7112003 <13anil67@gmail.com>
2024-03-04 12:30:01 +0530

    Day28

---
+++ DAY28/docstrings.py
@@ -0,0 +1,13 @@
+def square(n):
+  '''Takes in a number n, returns the square of n''' 
+  print(n**2)
+square(5)
+print(square.__doc__)  # this will print the doc statement 
+
+
+
+#''' bla bla''' this is the doc string and this is mot comment statement ......
+
+# def  square is the function ....
+
+
