ac9f7cb31e1e44072e54f138c231258930840f53
Daksh7112003 <13anil67@gmail.com>
2024-03-04 13:07:24 +0530

    Day30

--- DAY30/set.py
+++ DAY30/set.py
@@ -11,4 +11,8 @@ for value in info:
   print(value)
 
 
-  #Sets are unordered collection of data items. They store multiple items in a single variable. Set items are separated by commas and enclosed within curly brackets {}. Sets are unchangeable, meaning you cannot change items of the set once created. Sets do not contain duplicate items.
+  #Sets are unordered collection of data items.
+#    They store multiple items in a single variable.
+#   Set items are separated by commas and enclosed within curly brackets {}.
+#   Sets are unchangeable, meaning you cannot change items of the set once created. 
+#   Sets do not contain duplicate items.
