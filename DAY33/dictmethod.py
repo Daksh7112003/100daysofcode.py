faec3b71c01ad8fbf33cd15f2c75ed6933cb8457
Daksh7112003 <13anil67@gmail.com>
2024-03-04 17:01:44 +0530

    Day33

---
+++ DAY33/dictmethod.py
@@ -0,0 +1,16 @@
+ep1 ={122:45 , 123:90,
+     567:67}
+ep2={222:67 , 444:23}
+
+ep1.update(ep2)
+ #this will take the value of other dictionary also ...
+
+
+
+print(ep1)
+
+ep1.clear()   #this will empty the dict .....
+
+print(ep1)  
+
+
