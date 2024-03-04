9f3ba8937ca6953b5587d6bbfc90748a478c1b00
Daksh7112003 <13anil67@gmail.com>
2024-03-04 19:20:38 +0530

    day37

---
+++ DAY37/customerror.py
@@ -0,0 +1,8 @@
+a = int(input("Enter any value between 5 and 9"))
+
+if(a<5 or a>9):
+    raise ValueError("Value shoud be between 5 and 9")
+
+#this is the cusstom error , in which we take input from the user according to the need of the owner...........
+
+
