ecb9c50502aac297e93b35eb4b311499d423b2b5
Daksh7112003 <13anil67@gmail.com>
2024-03-12 13:29:51 +0530

    day42

---
+++ DAY42/daksh.py
@@ -0,0 +1,6 @@
+def welcome():
+    print("Hey you are welcome my friend ")
+
+
+harry = "A good boy "
+
---
+++ DAY42/import.py
@@ -0,0 +1,30 @@
+# import pandas
+# pandas.read_csv()
+
+
+import math
+math.floor(4.23)
+
+
+
+# from daksh import welcome,harry
+
+ #import shit .....
+
+
+
+# another way of importing.......
+
+from daksh import *
+
+
+
+
+
+import math 
+print(dir(math))
+
+welcome()
+print(harry)
+
+#this dir function will print all the inbuilt function of the math libraroy
